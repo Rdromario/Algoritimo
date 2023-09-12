3853998530a7bd42f90c5eac4a177b4e18421767
ROMARIO DOS SANTOS <romario.2919@aluno.pr.senac.br>
2023-09-12 08:28:21 -0300

    porcentagem

---
+++ src/main/java/com/mycompany/algoritimo/Porcentagem_de_um_determidado_numero.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.algoritimo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author romario.2919
+ */
+public class Porcentagem_de_um_determidado_numero {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        float numero, resultado;
+        System.out.println("Ecolha o numeruo para retirar a porcentagem de 10%");
+        
+        
+    }
+    
+}
